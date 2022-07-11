aebf0481cce85dffed76e4a0610ca634c67df149
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2022-07-11 00:54:13 -0300

    fix(home) new layout with large button

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -13,21 +13,37 @@ class HomePage extends StatelessWidget {
             Text('CONTADOR DE DIAS',
                 style: Theme.of(context).textTheme.headline1),
             const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/year');
-              },
-              child: const Text('Ano'),
-            ),
+            const ButtonHomeWidget(label: 'ANO', route: '/year'),
             const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/life');
-              },
-              child: const Text('Vida'),
-            ),
+            const ButtonHomeWidget(label: 'QUARTER', route: '/quarter'),
+            const SizedBox(height: 16),
+            const ButtonHomeWidget(label: 'VIDA', route: '/life'),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ButtonHomeWidget extends StatelessWidget {
+  final String label;
+  final String route;
+
+  const ButtonHomeWidget({
+    Key? key,
+    required this.label,
+    required this.route,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 350,
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.pushNamed(context, route);
+        },
+        child: Text(label),
       ),
     );
   }
